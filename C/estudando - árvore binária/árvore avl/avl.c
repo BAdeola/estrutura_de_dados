ba46d9c36095430a922cbdb2ac641b30e49d5925
Brayan Adeolá <brayanadeola@gmail.com>
2025-06-30 18:34:53 -0300

    modifeid

--- C/estudando - árvore binária/árvore avl/avl.c
+++ C/estudando - árvore binária/árvore avl/avl.c
@@ -39,3 +39,83 @@ short fatorDeBalancemaneto(No * no){
         return 0;
 }
 
+No * rotacaoEsquerda(No * r){
+    No *y, *f;
+
+    y = r->direita;
+    f = y->esquerda;
+
+    y->esquerda = r;
+    r->direita = f;
+
+    r->altura = maior(alturaDoNo(r->esquerda), alturaDoNo(r->direita)) + 1;
+    y->altura = maior(alturaDoNo(y->esquerda), alturaDoNo(y->direita)) + 1;
+
+    return y;
+}
+
+No * rotacaoDireita(No * r){
+    No *y, *f;
+
+    y = r->esquerda;
+    f = y->direita;
+
+    y->direita = r;
+    r->esquerda = f;
+
+    r->altura = maior(alturaDoNo(r->esquerda), alturaDoNo(r->direita)) + 1;
+    y->altura = maior(alturaDoNo(y->esquerda), alturaDoNo(y->direita)) + 1;
+
+    return y;
+}
+
+No* rotacaoDireitaEsquerda(No* r){
+    r->direita = rotacaoDireita(r->direita);
+    return rotacaoEsquerda(r);
+}
+
+No* rotacaoEsquerdaDireita(No* r){
+    r->esquerda = rotacaoEsquerda(r->esquerda);
+    return rotacaoDireita(r);
+}
+
+No* balancear(No* raiz){
+    short fb = fatorDeBalancemaneto(raiz);
+
+    //Rotação para esquerda
+    if (fb < -1 && fatorDeBalancemaneto(raiz->direita) <= 0)
+        raiz = rotacaoEsquerda(raiz);
+    
+    //Rotação direita
+    else if (fb > 1 && fatorDeBalancemaneto(raiz->esquerda) >= 0)
+        raiz = rotacaoDireita(raiz);
+
+    //Rotação dupla esquerda
+    else if (fb > 1 && fatorDeBalancemaneto(raiz->esquerda) < 0)
+        raiz = rotacaoEsquerdaDireita(raiz);
+    //Rotação dupla direita
+    else if (fb < -1 && fatorDeBalancemaneto(raiz->esquerda) > 0)
+        raiz = rotacaoDireitaEsquerda(raiz);
+    
+    return raiz;
+}
+
+No* inserir(No* raiz, int x){
+    if(raiz == NULL)
+        return novoNo(x);
+    else {
+        if(x < raiz->conteudo)
+            raiz->esquerda = inserir(raiz->esquerda, x);
+        else if(x > raiz->conteudo)
+            raiz->direita = inserir(raiz->esquerda, x);
+        else
+            printf("\nInserção não realizada!\nO elemento %d já existe.\n", x);
+    }
+
+    raiz->altura = maior(alturaDoNo(raiz->direita), alturaDoNo(raiz->esquerda)) + 1;
+
+    raiz = balancear(raiz);
+
+    return raiz;
+}
+
